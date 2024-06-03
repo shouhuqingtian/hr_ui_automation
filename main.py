13129bb475bcdb185ae1baf27afbab64b3515700
liu51 <519328539@qq.com>
2024-06-03 18:15:29 +0800

    第三次提交

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# 这是一个示例 Python 脚本。
+from openpyxl import load_workbook, Workbook
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+# 读取excel
+wb = load_workbook(r'C:\Users\liu51\Desktop\Test\00 资料\9_python自动化\testcase_api_wuye.xlsx')
 
+# 获取工作表
+sheet = wb['register']
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+# 读取所有字段数据
+for row in sheet.iter_rows(max_row=2, values_only=True):
+    print(row)
